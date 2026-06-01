c7509519366b7e56181dbcdaa2772b3ed3735ec3
Tsuchida Yukio <156447877+rydeen7907@users.noreply.github.com>
2026-06-01 13:39:47 +0900

    Change model name in run_sample_model.bat
    Updated model name in batch file and added comments.

--- run_Bonsai-1.7B-Q1_0.bat
+++ run_Bonsai-1.7B-Q1_0.bat
@@ -2,6 +2,12 @@ cd /d %~dp0
 timeout /t 1 >nul
 
 start http://127.0.0.1:8080
-llama-server.exe -m models/Bonsai-1.7B-Q1_0.gguf --port 8080
+llama-server.exe -m models/model_name.gguf --port 8080
 pause
 
+
+
+rem このコードで起動させる場合、10行目からのコメントは必ず削除してください。
+rem このファイルはサンプルで、かつ流用可能です。(ファイル名のモデルは実際にあります。)
+rem モデル名を書き換えるだけで、ブラウザが自動で起動します。(ファイル名も任意で変えてもOK)
+rem この場合、GUIはLlama.cppオリジナルで英語表記です。
